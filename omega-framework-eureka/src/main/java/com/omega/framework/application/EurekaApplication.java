8c1edce1b52f8ad2b21b04213c163f15b57cb406
uniseraph <zhengtao.wuzt@gmail.com>
2017-01-15 15:17:04 +0800

    using Eureka

--- omega-framework-eureka/src/main/java/com/omega/framework/application/EurekaApplication.java
+++ omega-framework-eureka/src/main/java/com/omega/framework/application/EurekaApplication.java
@@ -1,8 +1,7 @@
-package com.omega.framework.application.eureka;
+package com.omega.framework.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
