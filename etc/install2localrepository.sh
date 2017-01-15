6eff0bfeab61a562bdde999732dbad16ade47293
uniseraph <zhengtao.wuzt@gmail.com>
2017-01-15 16:49:42 +0800

    export pom

--- etc/install2localrepository.sh
+++ etc/install2localrepository.sh
@@ -5,7 +5,11 @@
 mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-datasource -Dversion=0.1 \
   -Dfile=../lib/omega-framework-datasource-0.1.jar  -DgeneratePom=true -Dpackaging=jar
 
+cp omega-framework-datasource.pom ~/.m2/repository/com/omega/framework/omega-framework-datasource/0.1/omega-framework-datasource-0.1.pom
 
 
 mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-task-common -Dversion=0.1 \
   -Dfile=../lib/omega-framework-task-common-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+
+
+cp omega-framework-task-common.pom ~/.m2/repository/com/omega/framework/omega-framework-task-common/0.1/omega-framework-task-common-0.1.pom
