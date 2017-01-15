fe127f1beeedb4cf889d559fd4285aa784a8ef44
jackychenb <jackychenb@jackychenbtekiMacBook-Pro.local>
2017-01-15 15:40:50 +0800

    Fix bug

--- etc/install2localrepository.sh
+++ etc/install2localrepository.sh
@@ -3,9 +3,9 @@
 
 
 mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-datasource -Dversion=0.1 \
-  -Dfile=lib/omega-framework-datasource-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+  -Dfile=../lib/omega-framework-datasource-0.1.jar  -DgeneratePom=true -Dpackaging=jar
 
 
 
 mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-task-common -Dversion=0.1 \
-  -Dfile=lib/omega-framework-task-common-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+  -Dfile=../lib/omega-framework-task-common-0.1.jar  -DgeneratePom=true -Dpackaging=jar
