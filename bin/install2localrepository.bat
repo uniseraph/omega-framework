1f9a0e8e81d180c008226a9218b1eb4da95eb8d4
zhengtao.wuzt <zhengtao.wuzt@alibaba-inc.com>
2017-01-15 11:50:16 +0800

    add bin

---
+++ bin/install2localrepository.bat
@@ -0,0 +1,40 @@
+
+
+ mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-eureka -Dversion=0.1 \
+  -Dfile=lib/omega-framework-eureka-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+
+
+
+ mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-configserver -Dversion=0.1 \
+  -Dfile=lib/omega-framework-configserver-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+
+
+
+
+ mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-datasource -Dversion=0.1 \
+  -Dfile=lib/omega-framework-datasource-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+
+
+
+ mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-task-common -Dversion=0.1 \
+  -Dfile=lib/omega-framework-task-common-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+
+
+
+
+ mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-taskserver -Dversion=0.1 \
+  -Dfile=lib/omega-framework-taskserver-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+
+
+
+ mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-admin-dashboard -Dversion=0.1 \
+  -Dfile=lib/omega-framework-admin-dashboard-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+
+
+
+ mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-admin-turbine -Dversion=0.1 \
+  -Dfile=lib/omega-framework-turbin-0.1.jar  -DgeneratePom=true -Dpackaging=jar
+
+
+ mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-admin-zipkin-server -Dversion=0.1 \
+  -Dfile=lib/omega-framework-zipkin-server-0.1.jar  -DgeneratePom=true -Dpackaging=jar
