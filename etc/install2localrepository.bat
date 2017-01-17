3e5f2bb5f6f5f83ea9fd1e7b54aad0f29f37e8b5
jackychenb <jackychenb@jackychenbtekiMacBook-Pro.local>
2017-01-17 16:00:54 +0800

    Adjust scripts

--- etc/install2localrepository.bat
+++ etc/install2localrepository.bat
@@ -1,13 +1,21 @@
 
+rmdir /S /Q "%USERPROFILE%\.m2\repository\com\omega\framework\omega-framework"
+rmdir /S /Q "%USERPROFILE%\.m2\repository\com\omega\framework\omega-framework-datasource"
+rmdir /S /Q "%USERPROFILE%\.m2\repository\com\omega\framework\omega-framework-task-common"
+
+
+
 md "%USERPROFILE%\.m2\repository\com\omega\framework\omega-framework\0.1"
 
 copy /y  omega-framework\pom.xml "%USERPROFILE%\.m2\repository\com\omega\framework\omega-framework\0.1\omega-framework-0.1.pom"
+
 
 
 mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-datasource -Dversion=0.1 \
   -Dfile=..\lib\omega-framework-datasource-0.1.jar  -DgeneratePom=true -Dpackaging=jar
 
 copy /y  omega-framework-datasource\pom.xml  "%USERPROFILE%\.m2\repository\com\omega\framework\omega-framework-datasource\0.1\omega-framework-datasource-0.1.pom"
+
 
 
 mvn install:install-file -DgroupId=com.omega.framework -DartifactId=omega-framework-task-common -Dversion=0.1 \
