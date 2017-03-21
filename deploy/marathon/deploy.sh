577a134881a18d60f65e4b961ae94cd3abd92aa8
uniseraph <zhengtao.wuzt@gmail.com>
2017-03-22 00:35:27 +0800

    deploy omega-framework in mesos/marathon

---
+++ deploy/marathon/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#deploy rabbitmq witch rabbitmq.mesos.consul
+
+curl -i -H 'Content-Type: application/json' -d @rabbitmq.json  kvm1:8080/v2/apps
+
+
+
+# deploy eureka cluster with eureka1/2/3.mesos.consul
+curl -i -H 'Content-Type: application/json' -d @eureka1.json  kvm1:8080/v2/apps
+curl -i -H 'Content-Type: application/json' -d @eureka2.json  kvm1:8080/v2/apps
+curl -i -H 'Content-Type: application/json' -d @eureka3.json  kvm1:8080/v2/apps
+
+
+
+# deploy configserver with configserver.mesos.consul
+curl -i -H 'Content-Type: application/json' -d @configserver.json  kvm1:8080/v2/apps
+
+
+
