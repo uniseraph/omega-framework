fd8f62ce2ff43e3bce0106a909ec3306abef6ed7
uniseraph <zhengtao.wuzt@gmail.com>
2017-03-29 21:32:23 +0800

    add aliyun hosts

--- deploy/native/pub-omega.sh
+++ deploy/native/pub-omega.sh
@@ -8,5 +8,5 @@ pssh -h hosts/$SERVICE_NAME -l root -i ' curl -X POST http://localhost:8080/shut
                                 sleep 5 ; \
                                java -Djava.security.egd=file:/dev/./urandom -jar /opt/omega-framework/lib/omega-framework-${SERVICE_NAME}-0.1.jar \
                                --server.port=8080  \
-                               --eureka.client.serviceUrl.defaultZone=http://eureka1:8080/eureka/ \
+                               --eureka.client.serviceUrl.defaultZone=http://eureka1:8080/eureka/,http://eureka2:8080/eureka,http://eureka3:8080/eureka \
                                '
