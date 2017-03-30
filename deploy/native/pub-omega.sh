07dfa2c79d598a3c7d05ef9c581a65dd298ff3de
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-03-30 19:42:42 +0800

    add normal parametes

--- deploy/native/pub-omega.sh
+++ deploy/native/pub-omega.sh
@@ -7,6 +7,8 @@ pscp -h hosts/$SERVICE_NAME  -l  admin  ./omega-framework/lib/omega-framework-${
 pssh -h hosts/$SERVICE_NAME -l admin -i ' curl -X POST http://localhost:8080/shutdown ;  \
                                 sleep 5 &&  \
                                java -Djava.security.egd=file:/dev/./urandom -jar /home/admin/omega-framework/lib/omega-framework-${SERVICE_NAME}-0.1.jar \
-                               --server.port=8080  \
+                               --spring.cloud.config.discovery.enabled=true  \
+                               --spring.cloud.config.profile=test  \
+                               --spring.cloud.config.label=master  \
                                --eureka.client.serviceUrl.defaultZone=http://eureka1:8080/eureka/,http://eureka2:8080/eureka,http://eureka3:8080/eureka \
                                '
