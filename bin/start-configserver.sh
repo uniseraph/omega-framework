44a236a03bcec44e1b1e9fde46cba80a1bba404c
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-04-17 15:31:27 +0800

    add trap function for signal term

--- bin/start-configserver.sh
+++ bin/start-configserver.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
 
+
+
+function cleanup() {
+
+    curl --connect-timeout 1 -fsSL -X POST http://localhost:8080/shutdown
+
+}
+trap cleanup SIGTERM
+
 java ${JAVA_OPTS}  -Djava.security.egd=file:/dev/./urandom -jar lib/omega-framework-configserver-0.1.jar $*
